b8211bcd4eba2bba10a28fec653c9abc602087dc
joylin <joylin@qnap.com>
2023-09-06 18:04:40 +0800

    fix

--- django4_book/django_web/mvote/mvote/urls.py
+++ django4_book/django_web/mvote/mvote/urls.py
@@ -21,5 +21,6 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('logout/', views.logout),
     path('accounts/', include('allauth.urls')),
 ]
--- django4_book/django_web/mvote/mysite/views.py
+++ django4_book/django_web/mvote/mysite/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html', locals())
+
+
+def logout(request):
+    auth.logout(request)
+    messages.add_message(request, messages.INFO, "成功登出了")
+    return redirect('/')
