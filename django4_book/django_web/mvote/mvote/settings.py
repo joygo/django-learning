bd4f3a12b902df8ccffddfa4336e468811193158
joylin <joylin@qnap.com>
2023-09-06 15:43:37 +0800

    update url and auth

--- django4_book/django_web/mvote/mvote/settings.py
+++ django4_book/django_web/mvote/mvote/settings.py
@@ -37,7 +37,29 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'mysite.apps.MysiteConfig',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.accout',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.facebook',
+    
+]
+
+
+SITE_ID = 1
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "joy820411@gmail.com"
+EMAIL_HOST_PASSWORD = "pledlrunghcstzjh"
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 MIDDLEWARE = [
