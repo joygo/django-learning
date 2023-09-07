74255f73b951088c17876431efb9ade3c82def3c
joylin <joylin@qnap.com>
2023-09-07 23:16:35 +0800

    update

--- django4_book/django_web/mvote/mvote/__init__.py
+++ django4_book/django_web/mvote/mvote/__init__.py
@@ -1,10 +1,10 @@
-import threading
+# import threading
 
-from mysite import task
+# from mysite import task
 
-# 启动定时任务线程
-def run_scheduler():
-    scheduler_thread = threading.Thread(target=task.weather_check)
-    scheduler_thread.start()
+# # 启动定时任务线程
+# def run_scheduler():
+#     scheduler_thread = threading.Thread(target=task.weather_check)
+#     scheduler_thread.start()
 
-run_scheduler()
+# run_scheduler()
