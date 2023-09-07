82ce2f6217da9c1bedd5d52f59e45a6152662369
joylin <joylin@qnap.com>
2023-09-07 16:49:15 +0800

    update

--- django4_book/django_web/mvote/mysite/views.py
+++ django4_book/django_web/mvote/mysite/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from mysite import models
 # Create your views here.
 
 def index(request):
+    all_polls = models.Poll.objects.all().order_by('-created_at')
+    paginator = Paginator(all_polls, 5)
+    p = request.GET.get('p')
+
+    try:
+        polls = paginator.page(p)
+    except PageNotAnInteger:
+        polls = paginator.page(1)
+    except EmptyPage:
+        polls = paginator.page(paginator.num_pages)
+
     return render(request, 'index.html', locals())
-
-
-def logout(request):
-    auth.logout(request)
-    messages.add_message(request, messages.INFO, "成功登出了")
-    return redirect('/')
